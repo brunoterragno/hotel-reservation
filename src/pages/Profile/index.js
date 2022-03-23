dedece1396d3ab47a701e4935d13c70a858995ab
vivianelocatelli <vivi_locatelli@hotmail.com>
2022-03-23 10:51:01 -0300

    Fix users/bookings response

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -19,43 +19,47 @@ import { AuthContext } from '../../context/user';
 const Profile = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectHotel, setSelectHotel] = useState([]);
-  const [userReservation, setUserReservation] = useState({});
+  const [userReservation, setUserReservation] = useState([]);
   const [loadingPage, setLoadingPage] = useState(true);
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    async function getFromApi() {
+    async function getReservationsFromApi() {
       const response = await api.get('users/bookings', {
         headers: {
           Authorization: user.token,
         },
       });
 
-      response.data.forEach((info) => {
-        getHotelFullInfo(info);
+      const hotelFullInfo = response.data.map((data) => {
+        return getHotelById(data);
       });
+
+      Promise.all(hotelFullInfo).then(
+        (values) => setUserReservation(values) || []
+      );
+
+      setLoadingPage(false);
     }
-    getFromApi();
+    getReservationsFromApi();
   }, [user]);
 
-  async function getHotelFullInfo(data) {
+  async function getHotelById(data) {
     const response = await api.get(`hotels/${data.hotel_id}`);
-    let userFullInfo = [
-      {
-        name: response.data.name,
-        image: response.data.image,
-        adress: response.data.adress,
-        ...data,
-      },
-    ];
-
-    setSelectHotel([response.data]);
-    setUserReservation(userFullInfo);
-    setLoadingPage(false);
+    return {
+      fullInfo: response.data,
+      ...data,
+    };
   }
 
-  function handleDelete(value) {
-    const hotel = userReservation.filter((d) => d.id !== value);
+  function handleSelect(index) {
+    const { fullInfo } = userReservation[index];
+    setSelectHotel([fullInfo]);
+    setIsOpen(!isOpen);
+  }
+
+  function handleDelete(index) {
+    const hotel = userReservation.filter((d, i) => i !== index);
     setUserReservation(hotel);
   }
 
@@ -83,19 +87,19 @@ const Profile = () => {
               {userReservation.map((h, i) => {
                 return (
                   <li key={i}>
-                    <img src={h.image} alt={h.name} />
+                    <img src={h.fullInfo.image} alt={h.fullInfo.name} />
                     <LiContent>
-                      <h3>{h.name}</h3>
+                      <h3>{h.fullInfo.name}</h3>
                       <Options>
                         <button
                           style={{ border: '1px solid #698f9e' }}
-                          onClick={() => setIsOpen(!isOpen)}
+                          onClick={() => handleSelect(i)}
                         >
                           <FaRegWindowRestore style={{ color: '#698f9e' }} />
                         </button>
                         <button
                           style={{ border: '1px solid #c10b0b' }}
-                          onClick={() => handleDelete(h.id)}
+                          onClick={() => handleDelete(i)}
                         >
                           <FaTrash style={{ color: '#c10b0b' }} />
                         </button>
